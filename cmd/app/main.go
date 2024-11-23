047c269e725e549b240913fc4f9b8991cebe5c87
febriaricandra <febriaricandraproduction@gmail.com>
2024-11-23 16:18:50 +0700

    fix cors allow access

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -66,7 +66,7 @@ func main() {
 
 	//CORs configuration
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8080"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
