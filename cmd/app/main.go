445471f4368cf320ab268fe4cd00f99239218bfe
febriaricandra <febriaricandraproduction@gmail.com>
2024-11-23 21:03:14 +0700

    fix cors headers

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -61,7 +61,7 @@ func main() {
 
 	//CORs configuration
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:5173/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"*", "Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
